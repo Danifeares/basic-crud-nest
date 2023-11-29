8a061982a2df494f6388ff6f409858d12c6d6ae4
Daniela Felipe Soares <daniela.felipe.soares@gmail.com>
2023-11-29 16:26:43 -0300

    configuração do projeto

---
+++ src/users/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user';
+
+@Injectable()
+export class UsersService {
+
+  private readonly users: User[] = [];
+
+  create(createUserDto: CreateUserDto) {
+    const newUser = new User(createUserDto.name, createUserDto.email);
+    this.users.push(newUser);
+    return newUser
+  }
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
